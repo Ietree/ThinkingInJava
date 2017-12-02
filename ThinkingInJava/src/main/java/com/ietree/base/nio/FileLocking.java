10c12c542bbd65d7462ca1ae708c4fd70e9362a0
Root <Dylansuns@hotmail.com>
2017-12-02 21:08:51 +0800

    更改成maven项目

---
+++ ThinkingInJava/src/main/java/com/ietree/base/nio/FileLocking.java
@@ -0,0 +1,27 @@
+package com.ietree.base.nio;
+
+import java.io.FileOutputStream;
+import java.nio.channels.FileLock;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 文件加锁
+ * 
+ * @author Root
+ */
+public class FileLocking
+{
+    public static void main(String[] args) throws Exception
+    {
+        FileOutputStream fos = new FileOutputStream("file.txt");
+        FileLock fl = fos.getChannel().tryLock();
+        if (fl != null)
+        {
+            System.out.println("Locked File");
+            TimeUnit.MILLISECONDS.sleep(100);
+            fl.release();
+            System.out.println("Released Lock");
+        }
+        fos.close();
+    }
+}
