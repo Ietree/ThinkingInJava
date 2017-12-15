4e5a4abcbe413208d84b3fbd39628bd14fb5a020
Root <Dylansuns@hotmail.com>
2017-12-15 08:23:20 +0800

    提交新代码

---
+++ ThinkingInJava/src/main/java/com/ietree/base/concurrency/TestBlockingQueues.java
@@ -0,0 +1,72 @@
+package com.ietree.base.concurrency;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+class LiftoffRunner implements Runnable {
+    private BlockingQueue<LiftOff> rockets;
+
+    public LiftoffRunner(BlockingQueue<LiftOff> queue) {
+        rockets = queue;
+    }
+
+    public void add(LiftOff lo) {
+        try {
+            rockets.put(lo);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                LiftOff rocket = rockets.take();
+                rocket.run();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Waking from take()");
+        }
+        System.out.println("Exiting LiftOffRunner");
+    }
+}
+
+public class TestBlockingQueues {
+    static void getkey() {
+        try {
+            new BufferedReader(new InputStreamReader(System.in)).readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void getkey(String message) {
+        System.out.println(message);
+        getkey();
+    }
+
+    static void test(String msg, BlockingQueue<LiftOff> queue) {
+        LiftoffRunner runner = new LiftoffRunner(queue);
+        Thread t = new Thread(runner);
+        t.start();
+        for (int i = 0; i < 5; i++) {
+            runner.add(new LiftOff(5));
+        }
+        getkey("Press 'Enter' (" + msg + ")");
+        t.interrupt();
+        System.out.println("Finished " + msg + " test");
+    }
+
+    public static void main(String[] args) {
+        test("LinkedBlockingQueue", new LinkedBlockingQueue<LiftOff>());
+        test("ArrayBlockingQueue", new ArrayBlockingQueue<LiftOff>(3));
+        test("LinkedBlockingQueue", new SynchronousQueue<LiftOff>());
+    }
+
+}
